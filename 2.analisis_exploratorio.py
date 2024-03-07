7fc851ae0f0a50d3845a2dd39cabbccb799ef4f5
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-03-06 22:32:17 -0500

    actualización exploración

--- 2.analisis_exploratorio.py
+++ 2.analisis_exploratorio.py
@@ -1,6 +1,6 @@
 ## Paquetes requeridos
 import pandas as pd
-import numpy as np
+import numpy as np 
 import country_converter as coco
 import pycountry_convert as pc
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression 
 from sklearn.ensemble import RandomForestClassifier
 import funciones as fn
 
@@ -26,135 +26,100 @@ df_pred = pd.read_csv(base16, sep = ',')
 df_pred.info() #la base de prediccion cuenta con 29 variables con 4410 datos en cada una4
 
 # Estucturacion variable objetivo (Attrition)
-df_bfinal['retirement_reason'].value_counts() # 650 empleados han abandonado de la empresa o han sido despedidos
-df_bfinal['retirement_reason'].isnull().sum() # 3760 empleados continuan trabajando en la empresa
 
-'''Se estructura la variable objetivo Attrition, la cual se define como la salida de un empleado de la empresa:
+'''Se estructurá la variable objetivo Attrition, la cual se define como la salida de un empleado de la empresa:
     si el empleado ha abandonado por razones de salario,estres u otros. se le asigna 'Yes'
     si el empleado continua trabajando(valor nulo) en la empresa o ha sido despedido(fired) se le asigna 'No' '''
     
 df_bfinal2 = df_bfinal.copy()
     
-df_bfinal2['Attrition'] = df_bfinal2['retirement_reason'].replace({'Salary':'yes', 'Others':'yes', 'Stress':'yes', 'Fired':'no'}) #reemplazamos valores segun la categoria retirement_reason
+df_bfinal2['Attrition'] = df_bfinal2['retirement_reason'].replace({'Salary':'yes', 'Others':'yes', 'Stress':'yes', 'Fired':'no'}).fillna('no')  #reemplazamos valores segun la categoria retirement_reason
 df_bfinal2['Attrition'].value_counts()
 df_bfinal2.isnull().sum()
 
-df_bfinal3 =df_bfinal2.copy()
-
-df_bfinal3.isnull().sum() 
-df_bfinal3['Attrition'] = df_bfinal3['Attrition'].fillna('no') #rellenamos valores faltantes con 'no'
-df_bfinal3['Attrition'].isnull().sum() #no hay valores faltantes
-
-# Priorización de exploración de variables NUMERICAS'
-
-###VER CLASE EN LA QUE SE HABLABA DE LAS FECHAS  DE PREDICCION Y ENTRENAMIENTO
-
 #Transformacion de variables
-"""Antes de realizar el modelo para priorizar las variables a explorar, vamos a realizar trannsformaciones de ciertas variables
-para que sean mas entendibles y para que el modelo pueda entenderlas mejor"""
+
 """variables a convertir a categoricas:
 Education, EnvironmentSatisfaction, JobInvolvement, JobSatisfaction, WorkLifeBalance,JobLevel"""
 
 variables = ['Education', 'EnvironmentSatisfaction', 'JobInvolvement', 'JobSatisfaction', 'WorkLifeBalance', 'JobLevel']
-df_fin = df_bfinal3.copy()
+df_fin = df_bfinal2.copy()
 
-df_fin = fn.transformaciontransformacion(df_fin, variables)
-
-df_fin.info() # base con variable  objetivo estructurada
+df_fin = fn.transformacion(df_fin, variables) # se transforman las variables a categoricas
 
 # Exploracion de variables que tengan nulos #
-#se eliminaran las variables con nulos ya que los modelos a realizar para la seleccion de variables no aceptan nulos
 
 df_fin.isnull().sum() 
 
 v_nulas = ['NumCompaniesWorked', 'TotalWorkingYears', 'EnvironmentSatisfaction', 'JobSatisfaction', 'WorkLifeBalance','mean_survery']
 
-df_no_null = fn.nulos(df_fin, v_nulas)
+df_no_null = fn.nulos(df_fin, v_nulas) # se eliminan valores nulos que representen menos del 10% de la base
 df_no_null.isnull().sum()
 
-df_no_null['Attrition'].value_counts()
+# Analisis de variables con mas del 10% de nulos #
 
-# Imputar valores nulos #
-# se imputaran los valores de la variable retirement_reason con 'no' ya que estos operarios suguen trabajando en la empresa
+df_no_null['retirement_reason'].value_counts() # empleados que abandonaron la empresa
+df_no_null['retirement_reason'] = df_no_null['retirement_reason'].fillna('NA') # se imputaran valores nulos con 'NA' lo cual implica que el empleado sigue trabajando en la empresa
 
-df_no_null['retirement_reason'] = df_no_null['retirement_reason'].fillna('NA')
+df_no_null2 = df_no_null.copy()
+df_no_null2 = df_no_null2.drop(['fecha_retiro','PercentSalaryHike'], axis=1) # se elimina la variable fecha_retiro ya que solo fue util para union de bases
+df_no_null2.info() # la base final cuenta con 27 variables y 4410 datos en cada una
 
-"""DEFINIR MODELO""" #ANTES HAY QUE ELIMINAR LOS NULOS
+# Exploración Variables Numercias #
+df_expl = df_no_null2.copy()
+df_expl = df_expl.select_dtypes(include=np.number) 
+df_expl.columns
 
-# MODELO WRAPPER para seleccion de variables a explorar detalladamente #
-## Backward selection
+# ANALISIS ATIPICOS #
+"""eliminar variables como employeeid, Percentajesalary . Disminuir el tamaño de letra a los boxplot
+o reagrupar las variables para que se vean mejor"""
 
-df_no_null['Attrition'] = df_no_null['Attrition'].replace({'yes':1, 'no':0}) #reemplazamos valores de la variable objetivo
+bxp1 = df_expl.iloc[:,:7] 
+bxp2 = df_expl.iloc[:,7:13]
+#bxp3 = df_expl.iloc[:]
 
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=bxp1)
+plt.title('Boxplot 1')
+plt.xlabel('Variables')
+plt.show()
 
-b_wrpper = df_no_null.copy() #copiamos la base sin nulos
-b_wrpper = b_wrpper.drop(['fecha_retiro','fecha_info'], axis = 1)
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=bxp2)
+plt.title('Boxplot 2')
+plt.xlabel('Variables')
+plt.show()
 
-#Volver variables dummies
-b_wrpper_d = pd.get_dummies(b_wrpper)
-b_wrpper_d.head()
+# Analisis Variable Objetivo #
 
+plt.figure(figsize=(8, 6))
+sns.countplot(data=df_expl, x='Attrition')
+plt.title('Attrition Count')
+plt.xlabel('Attrition')
+plt.ylabel('Count')
+plt.show()
 
-b_wrpper_int = b_wrpper.select_dtypes(include = ["number"]) # filtrar solo variables númericas
-b_wrpper_int = b_wrpper_int.drop('Attrition', axis = 1) #quitamos la variable objetivo
-y = b_wrpper_d['Attrition']#definimos variable objetivo
+# DIAGRAMAS VARIABLES UNIVARIADO #
 
-# Normalizacion variables numericas
+# Histogramas #
 
-b_wrpper_norm = b_wrpper_int.copy(deep = True)  # crear una copia del DataFrame
-scaler = MinMaxScaler() # asignar el tipo de normalización
-sv = scaler.fit_transform(b_wrpper_norm.iloc[:,:]) # normalizar los datos
-b_wrpper_norm.iloc[:,:] = sv # asignar los nuevos datos
-b_wrpper_norm.head()
+fig, axes = plt.subplots(3, 3, figsize=(15, 15))
+fig.suptitle('Histogramas Variables')
+sns.histplot(df_expl['Age'], kde=True, ax=axes[0, 0])
+sns.histplot(df_expl['TotalWorkingYears'], kde=True, ax=axes[0, 1])
+sns.histplot(df_expl['YearsWithCurrManager'], kde=True, ax=axes[0, 2])
+sns.histplot(df_expl['NumCompaniesWorked'], kde=True, ax=axes[1, 0])
+#sns.histplot(df_expl['PercentSalaryHike'], kde=True, ax=axes[1, 1])
+sns.histplot(df_expl['mean_survery'], kde=True, ax=axes[1, 2])
+sns.histplot(df_expl['TrainingTimesLastYear'], kde=True, ax=axes[2, 0])
+sns.histplot(df_expl['YearsSinceLastPromotion'], kde=True, ax=axes[2, 1])
+sns.histplot(df_expl['YearsAtCompany'], kde=True, ax=axes[2, 2])
+plt.show()
 
-#Estimador para seleccion de variables (modelo de regresion logistica)#
+# Analisis de correlacion entre variables numericas
 
-model = LogisticRegression() 
-
-b_var = fn.recursive_feature_selection(b_wrpper_norm, y, model, 7) # seleccionar 10 variables
-
-# Nuevo conjunto de datos a explorar
-b_varp = b_wrpper_int.iloc[:,b_var]
-b_varp.head(10)
-
-###------------------------------------------------------------------------------------#########
-
-# Create a Random Forest classifier
-rf = RandomForestClassifier()
-
-# Fit the model on the selected variables and the target variable
-df_class = df_no_null.copy()
-df_class['Attrition'] = df_class['Attrition'].replace({'yes':1, 'no':0})
-df_class = df_class.drop(['fecha_retiro','fecha_info','EmployeeID'], axis = 1)
-
-df_class_d = pd.get_dummies(df_class)
-
-df_class_d2 = df_class_d.drop('Attrition', axis = 1)
-
-y = df_class_d['Attrition']
-
-# Normalizar df_class_d2
-df_class_norm = df_class_d2.copy(deep = True)  # crear una copia del DataFrame
-scaler = MinMaxScaler() # asignar el tipo de normalización
-sv = scaler.fit_transform(df_class_norm.iloc[:,:]) # normalizar los datos
-df_class_norm.iloc[:,:] = sv # asignar los nuevos datos
-df_class_norm.head()
-
-rf.fit(df_class_norm, y)
-
-# Get the feature importances
-importances = rf.feature_importances_
-
-pd.DataFrame(importances, index = df_class_norm.columns, columns = ['importance']).sort_values('importance', ascending = False).head(14)
-
-
-### EXPLORACION DE DATOS CON LAS VARIABLES SELECCIONADAS ###
-
-"""Segun el modelo de regresion logistica y el modelo de random forest, las variables mas importantes para predecir la variable objetivo, son :
-    - Age
-    - TotalWorkingYears
-    - YearsWithCurrManager
-    - NumCompaniesWorked
-    - PercentSalaryHike
-    - mean_survery
-    - retirmen_reason""" # 10 numericas
+correlation = df_expl.corr()
+plt.figure(figsize=(15, 15))
+sns.heatmap(correlation, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title('Correlation Heatmap')
+plt.show()
