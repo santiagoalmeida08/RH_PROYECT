26a3f21fb262957c1890f2fdc3de4e1bd802d428
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-03-08 22:19:46 -0500

    explroacion.v2

--- 2.analisis_exploratorio.py
+++ 2.analisis_exploratorio.py
@@ -1,3 +1,4 @@
+
 ## Paquetes requeridos
 import pandas as pd
 import numpy as np 
@@ -14,6 +15,7 @@ from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression 
 from sklearn.ensemble import RandomForestClassifier
 import funciones as fn
+from sklearn.decomposition import PCA
 
 #Dataframes
 basefinal = 'https://raw.githubusercontent.com/santiagoalmeida08/RH_PROYECT/main/data_hr_proyect/basefinal.csv'
@@ -25,7 +27,7 @@ base16 = 'https://raw.githubusercontent.com/santiagoalmeida08/RH_PROYECT/main/da
 df_pred = pd.read_csv(base16, sep = ',')
 df_pred.info() #la base de prediccion cuenta con 29 variables con 4410 datos en cada una4
 
-# Estucturacion variable objetivo (Attrition)
+# Estucturacion variable objetivo (Attrition); es la variable objetivo ya que se busca predecir si un empleado abandonará la empresa o no
 
 '''Se estructurá la variable objetivo Attrition, la cual se define como la salida de un empleado de la empresa:
     si el empleado ha abandonado por razones de salario,estres u otros. se le asigna 'Yes'
@@ -37,23 +39,10 @@ df_bfinal2['Attrition'] = df_bfinal2['retirement_reason'].replace({'Salary':'yes
 df_bfinal2['Attrition'].value_counts()
 df_bfinal2.isnull().sum()
 
-#Transformacion de variables
+# Exploracion de variables que tengan nulos 
 
-"""variables a convertir a categoricas:
-Education, EnvironmentSatisfaction, JobInvolvement, JobSatisfaction, WorkLifeBalance,JobLevel"""
-
-variables = ['Education', 'EnvironmentSatisfaction', 'JobInvolvement', 'JobSatisfaction', 'WorkLifeBalance', 'JobLevel']
 df_fin = df_bfinal2.copy()
-
-df_fin = fn.transformacion(df_fin, variables) # se transforman las variables a categoricas
-
-# Exploracion de variables que tengan nulos #
-
-df_fin.isnull().sum() 
-
-v_nulas = ['NumCompaniesWorked', 'TotalWorkingYears', 'EnvironmentSatisfaction', 'JobSatisfaction', 'WorkLifeBalance','mean_survery']
-
-df_no_null = fn.nulos(df_fin, v_nulas) # se eliminan valores nulos que representen menos del 10% de la base
+df_no_null = fn.nulos(df_fin) # se eliminan valores nulos que representen menos del 10% de la base
 df_no_null.isnull().sum()
 
 # Analisis de variables con mas del 10% de nulos #
@@ -62,24 +51,47 @@ df_no_null['retirement_reason'].value_counts() # empleados que abandonaron la em
 df_no_null['retirement_reason'] = df_no_null['retirement_reason'].fillna('NA') # se imputaran valores nulos con 'NA' lo cual implica que el empleado sigue trabajando en la empresa
 
 df_no_null2 = df_no_null.copy()
-df_no_null2 = df_no_null2.drop(['fecha_retiro','PercentSalaryHike'], axis=1) # se elimina la variable fecha_retiro ya que solo fue util para union de bases
+df_no_null2 = df_no_null2.drop(['fecha_retiro'], axis=1) # se elimina la variable fecha_retiro ya que solo fue util para union de bases
 df_no_null2.info() # la base final cuenta con 27 variables y 4410 datos en cada una
 
 # Exploración Variables Numercias #
-df_expl = df_no_null2.copy()
-df_expl = df_expl.select_dtypes(include=np.number) 
-df_expl.columns
 
-# ANALISIS ATIPICOS #
-"""eliminar variables como employeeid, Percentajesalary . Disminuir el tamaño de letra a los boxplot
-o reagrupar las variables para que se vean mejor"""
+df_expl_num = df_no_null2.copy()
+df_expl_num = df_expl_num.select_dtypes(include=np.number).drop(['EmployeeID'], axis=1) # seleccionamos variables numericas y eliminamos EmployeeID ya que esta no aporta informacion relevante para la exploracion
+df_expl_num.info()
 
-bxp1 = df_expl.iloc[:,:7] 
-bxp2 = df_expl.iloc[:,7:13]
-#bxp3 = df_expl.iloc[:]
+df_expl_num.describe()
+"""En esta tabla se pueden observar varios datos importantes de cada variable como lo son :
+    - El 75% de los empleados tienen menos de 43 años lo cual nos indica una poblacion joven en la empresa
+    - El 50% de los empleados de la compañia han trabajaodo menos de 5 años en la empresa, esto nos podria indicar una cantidad siginificativa de empleados nuevos
+    - Se observa que en la compañia no se promueve de cargo con mucha frecuencia, esto se ve reflejado en la variable YearsSinceLastPromotion en la cual el 75% de los empleados no han sido promovido en los ultimos 3 años"""
+
+df_expl_num.columns
+#
+
+bxp1 = df_expl_num.iloc[:,:2] # partimos la base en 3 partes para poder visualizar los boxplot de manera mas clara
+bxp2 = df_expl_num.iloc[:,4:6]
+bxp3 = df_expl_num.iloc[:,6:10]
+
 
 plt.figure(figsize=(10, 6))
 sns.boxplot(data=bxp1)
+plt.title('Boxplot 1')
+plt.xlabel('Variables')
+plt.show()
+""" 1. Boxplot variable edad: se puede observar que la mayoria de los empleados tienen entre 30 y 40 años, admeas no se observan valores atipicos
+ 
+    2. Boxplot variable Distancia : se puede observar que el 75% de los empleados viven a menos de 10 km de la empresa, sin embargo el 15% restante vive a mas de 20 km de la empresa, lo cual podria ser un factor de desercion laboral"""
+
+# Analizaremos las variables stockoptionlevel, monthlyincome y percentSalaryHike aparte debido a que su escala es diferente a las demas variables
+plt.figure(figsize=(10, 6))
+sns.boxplot(data= df_expl_num[['StockOptionLevel']])
+plt.title('Boxplot 1')
+plt.xlabel('Variables')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+sns.boxplot(data= df_expl_num[['PercentSalaryHike']])
 plt.title('Boxplot 1')
 plt.xlabel('Variables')
 plt.show()
@@ -89,36 +101,55 @@ sns.boxplot(data=bxp2)
 plt.title('Boxplot 2')
 plt.xlabel('Variables')
 plt.show()
+""" 1. Boxplot variable total de años trabajando : el 75% de los empleados a trabajado menos de 15 años; esto se ve reflejadp con lo analizado en la edad
+       con lo cual puede exisir una alta correlacion entre estas dos variables 
+"""
 
-# Analisis Variable Objetivo #
-
-plt.figure(figsize=(8, 6))
-sns.countplot(data=df_expl, x='Attrition')
-plt.title('Attrition Count')
-plt.xlabel('Attrition')
-plt.ylabel('Count')
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=bxp3)
+plt.title('Boxplot 3')
+plt.xlabel('Variables')
+plt.show()
+""" Analisis de las variables relacionadas con el tiempo en la empresa: en los boxplots se puede observar que la mayoria de los empleados
+    lleva poco tiempo en la empresa por lo cual aun no se han promovido. Tanto en la variable años en la compañia como en la variable referente a 
+    la promocion hay datos atipicos correspondientes a empleados que llevan mas de 20 años en la empresa y aparentemente no han sido promovidos en un gran periodo de tiempo. 
+    Así entonces puede que la baja frecuencia de promociones sea un importante factor de desercion laboral"""
+    
+plt.figure(figsize=(10, 6))
+sns.boxplot(data= df_expl_num['MonthlyIncome']) 
+plt.title('Boxplot de Salario Mensual')
+plt.xlabel('Variables')
 plt.show()
 
+""" Boxplot de salario mensual: se puede observar que la mayoria de los empleados tienen un salario bajo, ademas se observan valores atipicos
+    correspondientes a empleados con salarios muy altos que probablemente sean los empleados que llevan mas tiempo en la empresa, esto podria ser un factor de desercion laboral ya que los empleados con salarios bajos
+    podrian sentirse desmotivados"""
+    
 # DIAGRAMAS VARIABLES UNIVARIADO #
 
 # Histogramas #
 
-fig, axes = plt.subplots(3, 3, figsize=(15, 15))
-fig.suptitle('Histogramas Variables')
-sns.histplot(df_expl['Age'], kde=True, ax=axes[0, 0])
-sns.histplot(df_expl['TotalWorkingYears'], kde=True, ax=axes[0, 1])
-sns.histplot(df_expl['YearsWithCurrManager'], kde=True, ax=axes[0, 2])
-sns.histplot(df_expl['NumCompaniesWorked'], kde=True, ax=axes[1, 0])
-#sns.histplot(df_expl['PercentSalaryHike'], kde=True, ax=axes[1, 1])
-sns.histplot(df_expl['mean_survery'], kde=True, ax=axes[1, 2])
-sns.histplot(df_expl['TrainingTimesLastYear'], kde=True, ax=axes[2, 0])
-sns.histplot(df_expl['YearsSinceLastPromotion'], kde=True, ax=axes[2, 1])
-sns.histplot(df_expl['YearsAtCompany'], kde=True, ax=axes[2, 2])
-plt.show()
+for column in df_expl_num.columns:
+    plt.figure(figsize=(8, 6))
+    sns.histplot(data=df_expl_num, x=column, kde=True)
+    plt.title(f'Histograma de {column}')
+    plt.xlabel(column)
+    plt.ylabel('Frecuancia')
+    plt.show()
+    
+"""La variable Age tiene una distribucion normal, con lo cual se puede inferir que la mayoria de empleados tienen entre 30 y 40 años"""
+"""La variable MonthlyIncome tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados tienen un salario bajo"""
+"""La variable YearsAtCompany tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados llevan pocos años en la empresa"""
+"""La variable YearsInCurrentRole tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados llevan poco tiempo en el cargo actual"""
+"""La variable YearsSinceLastPromotion tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados llevan poco tiempo desde su ultima promocion"""
+"""La variable YearsWithCurrManager tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados llevan poco tiempo con el actual gerente"""
+"""La variable TotalWorkingYears tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados llevan pocos años trabajando"""
+"""La variable YearsSinceLastPromotion tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados llevan poco tiempo desde su ultima promocion"""
+"""La variable YearsWithCurrManager tiene una distribucion asimetrica a la derecha, con lo cual se puede inferir que la mayoria de empleados llevan poco tiempo con el actual gerente"""
 
-# Analisis de correlacion entre variables numericas
+# Analisis de correlacion entre variables numericas #
 
-correlation = df_expl.corr()
+correlation = df_expl_num.corr()
 plt.figure(figsize=(15, 15))
 sns.heatmap(correlation, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
 plt.title('Correlation Heatmap')
