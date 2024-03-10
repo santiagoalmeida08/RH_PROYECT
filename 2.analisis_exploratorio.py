ae9299bf7ea0e0339a61758cdb748249c1315bc7
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-03-09 21:58:27 -0500

    se organiza el codigo y se exporta la base para seleccion de variables

--- 2.analisis_exploratorio.py
+++ 2.analisis_exploratorio.py
@@ -121,7 +121,6 @@ plt.title('Boxplot Edad y Distancia')
 plt.xlabel('Variables')
 plt.show()
 """ 1. Boxplot variable edad: se puede observar que la mayoria de los empleados tienen entre 30 y 40 años, admeas no se observan valores atipicos
- 
     2. Boxplot variable Distancia : se puede observar que el 75% de los empleados viven a menos de 10 km de la empresa, sin embargo el 15% restante vive a mas de 20 km de la empresa, lo cual podria ser un factor de desercion laboral"""
 
 # Analizaremos las variable monthlyincome aparte debido a que su escala es diferente a las demas variables
@@ -141,6 +140,7 @@ sns.boxplot(data=bxp2)
 plt.title('Boxplot de Variables relacionadas con el tiempo en la empresa')
 plt.xlabel('Variables')
 plt.show()
+
 """ Analisis de las variables relacionadas con el tiempo en la empresa: el 75% de los empleados a trabajado menos de 15 años; esto se ve reflejadp con lo analizado en la edad
     con lo cual puede exisir una alta correlacion entre estas dos variables .Tanto en la variable años en la compañia como en la variable referente a 
     la promocion hay datos atipicos correspondientes a empleados que llevan mas de 20 años en la empresa y aparentemente no han sido promovidos en un gran periodo de tiempo. """
@@ -156,8 +156,7 @@ plt.show()
 
 """Todas las variables que hacen referencia a los años de trabajo tienen una alta correlacion entre ellas;
 esto podria ser un problema para el modelo de regresion logistica, ya que podria haber multicolinealidad; 
-una solucion seria seleccionar las variables mas importantes para el modelo o aplicar un modelo de reduccion de dimensionalidad como PCA para reducir
-la cantidad de variables y evitar la multicolinealidad"""
+la solucion propuesta es realizar un modelo de seleccion de variables para conservar solo la variable mas representativa"""
 
 # Analisis Bivariado #
 
@@ -178,48 +177,35 @@ for column in df_biv_num.columns:
 
 """ En los boxplots se puede observar lo siguientes aspectos relevantes : 
     - Los empleados que abandonaron la empresa tienen menos edad que los que siguen trabajando
+    - En la variable referente al salario anual se puede observar que hubo desercion de empleados con un salario bastante alto, lo cual
+        nos llevaria a pensar que incluso estos empleados que probablemente lleven mucho tiempo en la empresa desiden abandonarla, siendo asi que la causa del abandono no sea el salario
     - Parte de los empleados que abandonaron el trabajo llevaban en promedio menos de 10 años en la empresa;
-      lo mismo sucede con la variable relacionada a total de años trabajados y años en la compañia; los empleados que 
-      abandonaron la empresa llevaban en todos los aspectos menos tiempo en la empresa que los que siguen trabajando
-    -Los empleados con salarios bajos tienden a abandonar la empresa"""
+     sin embargo encontramos el mismo comportamiento que en salario; lo cual nos quiere decir que hay una razon mas alla de la antiguedad que lleve el trabajador en la empresa
+    - Observando el boxplot de años con el mismo gente se nota un patron importante el cual hace referencia a que los empleados que llevan trabajando
+      mucho tiempo bajo el mismo jefe no han abandonado la empresa, lo cual puede indicar que el jefe puede influir en la desicion de abandonar la empresa"""
 
-df_pred.columns
-
-# Exploracion de variables categoricas #
-"""for i in df_no_null2[['StockOptionLevel', 'PercentSalaryHike', 'YearsSinceLastPromotion']]:
-    plt.figure(figsize=(8, 6))
-    sns.countplot(data=df_no_null2, x=i, hue='Attrition')
-    plt.title(f'{i} vs Attrition')
-    plt.xlabel(i)
-    plt.ylabel('Count')
-    plt.show()"""
-    
-
-"""En los graficos de barras se puede observar lo siguiente:
-    - Los empleados con stock options bajo o ninguno  tienden a abandonar la empresa
-    - Los empleados con aumentos salariales Medios tienden a abandonar la empresa (no es lo que se espera)
-    - Los empleados que no han sido promovidos en un largo periodo de tiempo tienden a abandonar la empresa"""
-    
 
 #Exploración variables categoricas
+
 df_expl_cat = df_no_null2.copy()
 df_no_null2.columns
 
 df_expl_cat = df_expl_cat.select_dtypes(include='object').drop(['fecha_info'], axis=1)
-
-
+df_expl_cat.columns
 #Analisis de las variables 
-
+    
 for column in df_expl_cat.columns:
-    base = df_expl_cat.groupby([column])[['Attrition']].count().reset_index().rename(columns ={'Attrition':'count'})
-    fig = px.pie(base, names=column, values='count', title= column)
-    xaxis_title = column
-    yaxis_title = 'Cantidad'
-    template = 'simple_white'
-    fig.show()
-## REEESTRUCTURAR JOBROL
+    if column != 'Attrition': # se excluye la variable objetivo ya que esta se analizara por separado
+        base = df_expl_cat.groupby([column])[['Attrition']].count().reset_index().rename(columns ={'Attrition':'count'})
+        fig = px.pie(base, names=column, values='count', title= column)
+        xaxis_title = column
+        yaxis_title = 'Cantidad'
+        template = 'simple_white'
+        fig.show()
+        
 
-"""Para la variable businesstravel se observa que la mayoria de datos se encuentran en la categoria viajar rara vez pero aun asi no es un peso lo suficientemente grande para descartar la variable"""
+
+"""Para la variable businesstravel se observa que la mayoria de datos se encuentran en la categoria viajar rara vez por lo cual se descarta la variable """
 """Para la variable department es importante analizar si en realidad es una variable representativa ya que el departamento de recursos humanos solo representa el 4,33% del total de los datos
 por lo cual esta variable puede descartarse y tomarse como no representativa"""
 """En education al tener un mayor numero de categorias es mas comun que existan categorias con poca representación por lo cual esto no sera motivo para descartar la variable"""
@@ -233,50 +219,26 @@ dificulte su interpretación a pesar de esto aun no se va tomar la decisión de 
 """Para la variable de numcompaniesworked no se puede tomar aun una decision de si es una variable representativa o no ya que a pesar de que tiene una categoria con un bajo porcentaje aun puede seguir
 aportando al modelo"""
 """"""
-"""En la variable TrainingTimesLastYear  se observa que hay una variable con un muy bajo porcentaje y otra con muy alto porcentaje por lo cual esta variable no es significativa"""
+"""El porcentaje de aumento anual se encuentra distribuido de manera equitativa entre las categorias por lo cual se sigue teniendo en cuenta la variable"""
+"""En cuanto a la variable de nivel de accion se observa que tiene categorias bien distribuidas por lo cual se sigue teniendo en cuenta la variable"""
 
+"""En la variable TrainingTimesLastYear  se observa que hay una variable con un muy bajo porcentaje y otra con muy alto porcentaje por lo cual esta variable no es significativa"""
+"""La variable años desde la ultima promocion se encuentra distribuida de manera equitativa entre las categorias por lo cual se sigue teniendo en cuenta la variable"""
 """Para el caso de la variable EnvironmentSatisfaction se tienen 4 variables cada una con una representatividad importante por lo cual se decide seguir trabajando con la variable"""
 """En la variable job satisfaction se puede observar que tiene un comportamiento muy similar al de la variable EnvironmentSatisfaction por lo cual se toma la decisión se trabjar solo con una
 de estas variables y en este caso sera la de job satisfaction debido a que esta mas enfocada a la satisfaccion con el  puesto o el cargo y esto podria influir en mayor medida en la decisión de 
 renunciar al empleo """
+"""La mayoria de los datos de job involvement se encuentran en la categoria, sin embargo no es informacion ssuficionete para descartar la variable"""
 """En lo relacionado con la variable work life balance se observa que la mayoria de datos se distribuyen entre las categorias de satisfecho e insatisfecho por lo cual consideramos que esta variable si podria 
 ser representativa dentro de modelo"""
 """Para la variable job involvement hay una categoria que cuenta con mas de la mitad de los datos pero de igual forma aun no se descartara esta variable"""
 """A pesar de que la variable performance rating tiene mas del 80% de los datos en la categoria de un rendimiento bajo no se va descartar aun esta variable porque podria ser importante 
 para el desarrollo del modelo"""
 
-
-#Grafico retirement reason
-base = df_expl_cat.groupby(['retirement_reason'])[['Attrition']].count().reset_index().rename(columns ={'Attrition':'count'})
-fig = px.pie(base, names='retirement_reason', values='count', title= '<b>Retirement Reason<b>')
-xaxis_title = 'retirement_reason'
-yaxis_title = 'Cantidad'
-template = 'simple_white'
-fig.show()
-
 """Para retirement reason se puede observar que la mayoria  datos se encuentran en la categoria NA ya que son aquellas personas que no han renunciado por lo cual no se tiene una razon de despido"""
 
 
-#Relación de variables categoricas
-
-df_expl_cat.boxplot("Attrition","Gender",figsize=(15,15),grid=False)
-
-
-from scipy.stats import chi2_contingency
-import pandas as pd
-import seaborn as sns
-
-for column in df_expl_cat.columns:
-    # Crear una tabla de contingencia
-    tabla_contingencia = pd.crosstab(df_expl_cat[column], df_expl_cat['Attrition'])
-
-    # Realizar la prueba chi-cuadrado
-    chi2, p, dof, expected = chi2_contingency(tabla_contingencia)
-
-    print(f"Chi-cuadrado: {chi2}, P-valor: {p}, {column}")
-    """Si p-valor ≤ α, rechazas la hipótesis nula. Esto indica que hay suficiente evidencia para afirmar que existe una asociación significativa entre las variables categóricas.
-Si p-valor > α, no rechazas la hipótesis nula. Esto sugiere que no hay suficiente evidencia para afirmar que existe una asociación entre las variables, y cualquier diferencia 
-observada podría atribuirse al azar."""
+#Analisis Bivariado de variables categoricas #
 
 for column in df_expl_cat.columns:
     # Crear el gráfico de conteo
@@ -291,37 +253,32 @@ for column in df_expl_cat.columns:
     plt.show()
 
 #INTERPRETACIÓN
-"""No se observa una relacion significativa entre la variable businesstravel y la variable respuesta """
-"""Para la variable department tampoco logra verse una relación entre esta y la variable respuesta """
-"""En education tampoco hay variación entre las personas que si renuncian y las que no, en realidad la variacion que se observa es por la diferencia en la cantidad de datos"""
-"""Para la variable education field  hay muy poca variación al compararla con las categorias de las variables respuesta"""
-"""La variable gender tampoco presenta una variación, al igual que en las variables anteriores las varaciones que observan es por la cantidad de datos"""
-"""En cuanto a las categorias de la variable job level cuando estas se comparan con la variable respuesta no se ve una relacion significativa"""
-"""La variable job role cuenta con 9 categorias por lo cual es mas dificil identificar una relacion entre las variables, pero de igual forma se puede afirmar que esta variable
-no influye en la variable respuesta"""
-"""En la variable marital status si se puede observar cierta influencia en la variable respuesta teniendo que las personas que mas renuncian son las que estan solteras"""
-"""El numero de compañias en el que ha trabajado tampoco  refleja influencia en la variable respuesta"""
-"""En la variable TrainingTimesLastYear  no se observa relacion con la variable respuesta y de igual forma las variaciones se deben a la cantidad de datos"""
 
-"""Para el caso de la variable EnvironmentSatisfaction si se observa que podria haber cierte relacion con la variable respuesta, renunciando mas las personas que se encuentran en la categoria de 
-muy insatisfecho tal como era de esperarse"""
-"""En la variable job satisfaction se puede observar que si hay una influencia y que las personas que mas renuncian son las que se encuentran en las categorias de satisfecho y muy satisfecho """
-"""En lo relacionado con la variable work life balance no se observa relacion con la variable respuesta"""
-"""Para la variable job involvement no se observa relacion alguna con la variable respuesta"""
-"""Para la variable performance rating se observa que no hay variacion pero de igual forma las personas que mas renuncian son aquellas que tienen un rendimiento bajo
-aunque esto tambien puede deberse a que estas representan la mayor parte de los datos"""
-"""Tal como se esperaba para la variable retirement reason solo se tienen 3 categorias para las personas que si se retiraron organizadas de menor a mayor se encuentra el estres, el salario y otras razones"""
+""" 1.No se observa una relacion significativa entre la variable businesstravel y la variable respuesta 
+    2.Para la variable department tampoco logra verse una relación entre esta y la variable respuesta
+    3.En education tampoco hay variación entre las personas que si renuncian y las que no, en realidad la variacion que se observa es por la diferencia en la cantidad de datos
+    4.Para la variable education field  hay muy poca variación al compararla con las categorias de las variables respuesta
+    5.La variable gender tampoco presenta una variación, al igual que en las variables anteriores las varaciones que observan es por la cantidad de datos
+    6.En cuanto a las categorias de la variable job level cuando estas se comparan con la variable respuesta no se ve una relacion significativa
+    7.La variable job role cuenta con 9 categorias por lo cual es mas dificil identificar una relacion entre las variables, pero de igual forma se puede afirmar que esta variable
+    no influye en la variable respuesta
+    8.En la variable marital status si se puede observar cierta influencia en la variable respuesta teniendo que las personas que mas renuncian son las que estan solteras
+    9.El numero de compañias en el que ha trabajado tampoco  refleja influencia en la variable respuesta
+    10.En la variable TrainingTimesLastYear  no se observa relacion con la variable respuesta y de igual forma las variaciones se deben a la cantidad de datos
+    11.Para el caso de la variable EnvironmentSatisfaction si se observa que podria haber cierte relacion con la variable respuesta, renunciando mas las personas que se encuentran en la categoria de 
+    muy insatisfecho tal como era de esperarse
+    12.En la variable job satisfaction se puede observar que si hay una influencia y que las personas que mas renuncian son las que se encuentran en las categorias de satisfecho y muy satisfecho 
+    13.En lo relacionado con la variable work life balance no se observa relacion con la variable respuesta
+    14.Para la variable job involvement no se observa relacion alguna con la variable respuesta
+    15.Para la variable performance rating se observa que no hay variacion pero de igual forma las personas que mas renuncian son aquellas que tienen un rendimiento bajo
+    aunque esto tambien puede deberse a que estas representan la mayor parte de los datos
+    16.Tal como se esperaba para la variable retirement reason solo se tienen 3 categorias para las personas que si se retiraron organizadas de menor a mayor se encuentra el estres, el salario y otras razones"""
+
+"""En conclusion las variables categoricas que de alguna forma podrian tener significancia en el modelo son:
+    WorkLifeBalance, JobSatisfaction, JobSatisfaction,EnviromentSatisfaction,YearsSionceLastPromotion,MaritialStatus"""
 
 
 
-"""VARIABLES IMPORTANTES SEGUN EL ANALISIS EXPLORATORIO
-- Age
--MonthlyIncome
--TotalWorkingYears
--YearsAtCompany
+base_seleccion = df_no_null2.copy()
 
--Education
--EducationField
--JobLevel
--JobRole
--NumCompaniesWorked"""
+base_seleccion.to_csv('data_hr_proyect/base_seleccion.csv', index= False)
