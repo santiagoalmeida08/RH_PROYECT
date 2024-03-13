a365a0e046e8e0df491e6fe6e86d42ea91325ba3
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-03-13 17:19:05 -0500

    actualización comentarios

--- 4.modelos.py
+++ 4.modelos.py
@@ -2,24 +2,19 @@
 
 #Paquetes para manejo de datos
 import pandas as pd 
-import numpy as np
-from sklearn.feature_selection import SelectFromModel
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.metrics import accuracy_score,confusion_matrix, ConfusionMatrixDisplay, f1_score
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OrdinalEncoder
-from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import cross_val_predict, cross_val_score, cross_validate
 from sklearn.model_selection import RandomizedSearchCV
-from xgboost import XGBClassifier
+from sklearn.preprocessing import RobustScaler
+import funciones as fn
+
+
 # Importar el dataframe deputado en el analisis exploratorio
 
 data_seleccion= 'https://raw.githubusercontent.com/santiagoalmeida08/RH_PROYECT/main/data_hr_proyect/base_seleccion2.csv'
@@ -30,8 +25,8 @@ df1 = df.copy()
 df1 = df1.drop('EmployeeID', axis = 1) # Eliminar columna EmployeeID ya que no aporta información relevante
 df1.info()#Visualizar tipo de datos y verificar variables 
 
-df1['JobLevel'] = df1['JobLevel'].astype('object')
-df1['Attrition'] = df1['Attrition'].astype('object')
+df1['JobLevel'] = df1['JobLevel'].astype('object') #transformar JobLevel a variable categorica
+df1['Attrition'] = df1['Attrition'].astype('object') #transformar Attrition a variable categorica ya que en el analisis la usamos como numerica para analisar la correlación
 df1['Attrition'].value_counts()# Cambiar valores de Attrition a 1 y 0
 
 #Cambiamos el formato de las variables a int64 para poder trabajar con ellas en un solo formato
@@ -51,29 +46,8 @@ list_dd = ['Department','Education','EducationField','JobRole','MaritalStatus','
 
 # Codificación de variables 
 
-def encode_data(df, list_le, list_dd):
-    df_encoded = df1.copy()
-    
-    #Get dummies
-    df_encoded=pd.get_dummies(df_encoded,columns=list_dd)
-    
-    # Ordinal Encoding
-    #oe = OrdinalEncoder()
-    #for col in list_oe:
-    #    df_encoded[col] = oe.fit_transform(df_encoded[[col]])
-    
-    # Label Encoding
-    le = LabelEncoder()
-    for col in list_le:
-        df_encoded[col] = le.fit_transform(df_encoded[col])
-    
-    return df_encoded
-
-df_encoded = encode_data(df1, list_le,list_dd)
-
+df_encoded = fn.encode_data(df1, list_le,list_dd)
 df3 = df_encoded.copy()
-
-
 df3.dtypes
 
 for i in df3.columns:
@@ -83,56 +57,33 @@ for i in df3.columns:
         pass
 
 #Normalizacion
-
 v_num = []
 for col in df3.columns:
     if df3[col].dtypes == "int64":
         v_num.append(col)
-
-from sklearn.preprocessing import RobustScaler
-from sklearn.preprocessing import MinMaxScaler 
-from sklearn.preprocessing import StandardScaler
-
-scaler = RobustScaler()
+scaler = RobustScaler() # se usa robust scaler en el escalado para evitar la influencia de outliers
 for col in v_num:
     df3[[col]] = scaler.fit_transform(df3[[col]])
 
+
 df3.head()
 
-X_esc = df3.drop('Attrition', axis = 1)
-y = df3['Attrition']
+X_esc = df3.drop('Attrition', axis = 1) # elimino la variable objetivo para conservar solo las variables predictoras
+y = df3['Attrition'] # variable objetivo
 
 # Selecccion de modelos #
-
 model_gb = GradientBoostingClassifier()
 model_arb = DecisionTreeClassifier() 
 model_log = LogisticRegression( max_iter=1000, random_state=42)
 model_rand = RandomForestClassifier()
-
-
 modelos  = list([model_gb,model_arb,model_log,model_rand])
 
 # Seleccion de variables con base a los modelos seleccionados
-def sel_variables(modelos,X,y,threshold):
-    
-    var_names_ac=np.array([])
-    for modelo in modelos:
-   
-        modelo.fit(X,y)
-        sel = SelectFromModel(modelo, prefit=True,threshold=threshold)
-        var_names= modelo.feature_names_in_[sel.get_support()]
-        var_names_ac=np.append(var_names_ac, var_names)
-        var_names_ac=np.unique(var_names_ac)
-    
-    return var_names_ac
 
-
-var_names= sel_variables(modelos,X_esc,y,threshold="3.1*mean") 
+var_names= fn.sel_variables(modelos,X_esc,y,threshold="3.1*mean") 
 var_names.shape
-
 # Al utiizar numeros menores se aceptaban mas variables sin embargo el desempeño seguia siendo el mosmo por lo cual
 # Se utilizo un treshold de 2.8 en el cual se traba con 8 variables las cuales aportan significia a los modelos
-
 
 df_var_sel = df3[var_names]
 df_var_sel.info()
@@ -140,63 +91,38 @@ df_var_sel.info()
 # Division data en train y test kfold-croos-validation #
 
 df4 = df_var_sel.copy()
-
-from sklearn.model_selection import cross_val_score
-from sklearn.pipeline import make_pipeline
-
-
-
-def medir_modelos(modelos,scoring,X,y,cv):
-    os = RandomOverSampler()
-    metric_modelos=pd.DataFrame()
-    for modelo in modelos:
-        pipeline = make_pipeline(os, modelo)
-        scores=cross_val_score(modelo,X,y, scoring=scoring, cv=cv )
-        pdscores=pd.DataFrame(scores)
-        metric_modelos=pd.concat([metric_modelos,pdscores],axis=1)
-    
-    metric_modelos.columns=["gard_boost","decision_tree","random_forest","reg_logistic"]
-    return metric_modelos
-
-f1sco_df = medir_modelos(modelos,"f1",X_esc,y,15)  #se definen 15 iteraciones para tener mejor visión del desempeño en el boxplot
-f1dco_var_sel = medir_modelos(modelos,"f1",df4,y,15)
+f1sco_df = fn.medir_modelos(modelos,"f1",X_esc,y,15)  #se definen 15 iteraciones para tener mejor visión del desempeño en el boxplot
+f1dco_var_sel = fn.medir_modelos(modelos,"f1",df4,y,15)
 
 f1s=pd.concat([f1sco_df,f1dco_var_sel],axis=1) 
 f1s.columns=['rlog', 'dtree', 'rforest', 'gboosting',
        'rl_Sel', 'dt_sel', 'rf_sel', 'gb_Sel']
-
 f1s.plot(kind='box') # Boxplot de f1 score para cada modelo con todas las variables y con las variables seleccionadas
+f1s.mean()  # Media de rendimiendo para cada variable 
+
 #En el boxplot de rendimiento observamos que los modelos con mayor rendimiento son descision tree clasifier y gardient boosting, con un rendimiento medio de
 #0.6 y 0.7 respectivamente. Usaremos gardient boosting ya que es el modelo con mejor rendimiento y aunque su interpretabilidad no es la mejor; ademas
 # de un mejor rendimiento tiene una menor varianza en el rendimiento.
 
-f1s.mean()  # Media de rendimiendo para cada variable 
-
-#matriz confusion para dt_sel
+#Matriz confusion para dt_sel
 model_arb = DecisionTreeClassifier(max_depth=4, random_state=42)
 model_arb.fit(df4,y)
-
 y_pred = model_arb.predict(df4)
 cm = confusion_matrix(y, y_pred)
 cmd = ConfusionMatrixDisplay(cm, display_labels=model_arb.classes_)
 cmd.plot()
 
-#matriz confusion para gb_Sel
-model_gb = GradientBoostingClassifier()
-model_gb.fit(df4,y)
-
-y_pred = model_gb.predict(df4)
-cm = confusion_matrix(y, y_pred)
-cmd = ConfusionMatrixDisplay(cm, display_labels=model_gb.classes_)
-cmd.plot()
-
-
-#Definicion de parametros para gardient boosting
-
-parameters = { }
-
+#Definicion de parametros para DecisionTreeClassifier
+parameters = {
+    'criterion': ['gini', 'entropy'],
+    'splitter': ['best', 'random'],
+    'max_depth': [5, 10, 15],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['auto', 'sqrt', 'log2'] #max_feauters se refiere a la cantidad de variables que se toman en cuenta para hacer la mejor division
+}
 # create an instance of the randomized search object
-r1 = RandomizedSearchCV(GradientBoostingClassifier(), parameters, cv=5, n_iter=100, random_state=42, n_jobs=-1, scoring='accuracy') 
+r1 = RandomizedSearchCV(DecisionTreeClassifier(), parameters, cv=5, n_iter=100, random_state=42, n_jobs=-1, scoring='accuracy') 
 
 r1.fit(df4,y) #df4 es el dataframe con las variables seleccionadas
 
@@ -229,4 +155,11 @@ train_test_rf.columns=['train_score','test_score']
 train_test_rf
 
 train_test_rf["test_score"].mean()
+
+#haz una matriz de confusion para el modelo final con la data de cross validation
+y_pred = cross_val_predict(rf_final,df4,y,cv=5)
+cm = confusion_matrix(y, y_pred)
+cmd = ConfusionMatrixDisplay(cm, display_labels=rf_final.classes_)
+cmd.plot()
+
 
--- funciones.py
+++ funciones.py
@@ -1,5 +1,14 @@
 """Funciones que se utilizaran en el proyecto"""
-from sklearn.feature_selection import RFE
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_predict, cross_val_score, cross_validate 
+from sklearn.feature_selection import SelectFromModel
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
+
 
 #FUNCION 1#
 def transformacion(df, variables):
@@ -7,7 +16,6 @@ def transformacion(df, variables):
         df[i] = df[i].astype('object')
     return df
 
-#FUNCION 2#
 # FUNCION NULOS#
 def nulos(df):
     for i in df.columns:
@@ -19,20 +27,53 @@ def nulos(df):
 
 # FUNCION 3# 
 
-def recursive_feature_selection(X,y,model,k): #model=modelo que me va a servir de estimador para seleccionar las variables
-                                              # K = numero de variables a seleccionar
-  rfe = RFE(model, n_features_to_select=k, step=1) # step=1 significa que se eliminara una variable en cada iteracion
-  fit = rfe.fit(X, y) # ajustar el modelo
-  b_var = fit.support_ # seleccionar las variables
-  print("Num Features: %s" % (fit.n_features_)) # numero de variables seleccionadas
-  print("Selected Features: %s" % (fit.support_)) # variables seleccionadas
-  print("Feature Ranking: %s" % (fit.ranking_)) # ranking de las variables
-
-  return b_var 
-
-#Funcion 4#
-
 def escala(df,variables):
     for i in variables:
         df[i] = df[i].replace({'1.0':'Muy insatisfecho', '2.0':'Insatisfecho', '3.0':'Satisfecho', '4.0':'Muy satisfecho'})
     return df
+
+# FUNCION 4#
+def sel_variables(modelos,X,y,threshold):
+    
+    var_names_ac=np.array([])
+    for modelo in modelos:
+   
+        modelo.fit(X,y)
+        sel = SelectFromModel(modelo, prefit=True,threshold=threshold)
+        var_names= modelo.feature_names_in_[sel.get_support()]
+        var_names_ac=np.append(var_names_ac, var_names)
+        var_names_ac=np.unique(var_names_ac)
+    
+    return var_names_ac
+
+# FUNCION 5#
+
+
+def medir_modelos(modelos,scoring,X,y,cv):
+    os = RandomOverSampler()
+    metric_modelos=pd.DataFrame()
+    for modelo in modelos:
+        pipeline = make_pipeline(os, modelo)
+        scores=cross_val_score(modelo,X,y, scoring=scoring, cv=cv )
+        pdscores=pd.DataFrame(scores)
+        metric_modelos=pd.concat([metric_modelos,pdscores],axis=1)
+    
+    metric_modelos.columns=["gard_boost","decision_tree","random_forest","reg_logistic"]
+    return metric_modelos   
+
+# FUNCION 6#
+def encode_data(df, list_le, list_dd,list_oe):    
+    #Get dummies
+    df_encoded=pd.get_dummies(df_encoded,columns=list_dd)
+    
+    # Ordinal Encoding
+    oe = OrdinalEncoder()
+    for col in list_oe:
+        df_encoded[col] = oe.fit_transform(df_encoded[[col]])
+    
+    # Label Encoding
+    le = LabelEncoder()
+    for col in list_le:
+        df_encoded[col] = le.fit_transform(df_encoded[col])
+    
+    return df_encoded
